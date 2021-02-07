dacab94a555408a535ae37249350db68208f8400
unknown <wonjiprog@gmail.com>
2021-02-07 18:00:33 +0900

    aws rds로 회원가입 구현

---
+++ algorithm/index.js
@@ -0,0 +1,12 @@
+const students = ['woorim', 'minjae', 'sanjoon'];
+
+console.log(students[0]);
+//결과는 woorim 을 출력함
+
+const grades = [ ['wonji', 'A'], ['jiwon', 'B'], ['soongu', 'C']];
+
+console.log(grades[0][0]);
+// 결과는 wonji를 출력함
+
+console.log(grades[0][1]);
+// 결과는 A를 출력함
---
+++ algorithm/상하좌우.js
@@ -0,0 +1,8 @@
+const name = ['wonji', 'dong' , 'hyun'];
+let b = 0;
+for (let a of name) {
+  if (a === 'wonji') {
+      b += 1;
+  }
+}
+console.log(b);
